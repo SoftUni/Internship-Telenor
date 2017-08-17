8a937b83fd893c7dec6fa3df7a42bc5cb1700083
Alex <alexchobanov@gmail.com>
2017-08-18 02:05:59 +0300

    Implemented resizeable IFrame.

--- scripts/controllers/details-controller.js
+++ scripts/controllers/details-controller.js
@@ -9,17 +9,34 @@ define(detailsDependencies, (data, detailsView) => {
 		let intern = data.getSingleIntern(e.target.value)
 
 		$('#root').html(detailsView(intern))
+		resizeFrame()
 	}
 	detailsController.attachListeners = listController => {
 		showListListener(listController, detailsController)
+		resizeListener()
 	}
 
 	return detailsController
 })
 
+// Listeners
 function showListListener(listController, detailsController) {
 	$('.intern-list-btn').on('click', null, null, () => {
 		listController.render()
 		listController.attachListeners(detailsController)
 	})
+}
+
+function resizeListener() {
+	$(window).on('resize', null, null, () => {
+		resizeFrame()
+	})
+}
+
+// Listener dependencies
+function resizeFrame() {
+	let playerFrame = $('iframe')
+	let frameWidth = playerFrame.width()
+	let frameHeight = Math.trunc(frameWidth / 1.777)
+	playerFrame.height(frameHeight)
 }
