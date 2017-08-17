f151f3fc68aec25c9b3d0bdba36a6acccb15f119
Alex <alexchobanov@gmail.com>
2017-08-17 22:27:22 +0300

    Expanded upon my PoC. Now we have index file and one separate controller for each page.

--- scripts/views/details.js
+++ scripts/views/details.js
@@ -1,9 +1,9 @@
 define(() => function(intern) {
     return `
-    <div>
-        <p>Id: ${intern.id}</p>
-        <p>Name: ${intern.name}</p>
-        <img src="./${intern.picture}" alt="intern picture">
-        <button class="intern-list-btn">Back</button>
-    </div>`
+        <div>
+            <p>Id: ${intern.id}</p>
+            <p>Name: ${intern.name}</p>
+            <img src="./${intern.picture}" alt="intern picture" />
+            <button class="intern-list-btn">Back</button>
+        </div>`
 })
