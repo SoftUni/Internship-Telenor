ee896e82979f4f0d8ca57ba492c7d03ba72363dc
Silvave <todor.vasilev@yahoo.com>
2017-08-18 03:54:42 +0300

    Small changes to list view

--- scripts/views/list.js
+++ scripts/views/list.js
@@ -1,33 +1,35 @@
 define(['../templates/intern-card-template.js'], internCardTemplate => {
     return (interns) => {
-        let generateListOfAllCards = `
-            <div class="hub-image-wrapper">
-                <img src="./images/hub-by-telenor-v3.png" alt="telenor image hub" class="hub-image">
-            </div>
-            <section class="info-box">
-                <div class="info-text-wrapper">
-                    <h4 class="info-title">
-                        Lorem Ipsum is simply dummy text of the print
-                    </h4>
-                    <p class="info-text">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
-                    </p>
+        let generateListView = `
+            <div class="list-container">
+                <div class="hub-image-wrapper">
+                    <img src="./images/hub-by-telenor-v3.png" alt="telenor image hub" class="hub-image">
                 </div>
-                <div class="btn-wrapper">
-                    <button class="apply-btn">Кандидатствай</button>
-                </div>
-            </section>
-            <section class="intern-cards">
-                <h3 class="title">
-                    Научете повече от други стажанти
-                </h3>`
+                <section class="info-box">
+                    <div class="info-text-wrapper">
+                        <h4 class="info-title">
+                            Lorem Ipsum is simply dummy text of the print
+                        </h4>
+                        <p class="info-text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
+                        </p>
+                    </div>
+                    <div class="btn-wrapper">
+                        <button class="apply-btn">Кандидатствай</button>
+                    </div>
+                </section>
+                <section class="intern-cards">
+                    <h3 class="title">
+                        Научете повече от други стажанти
+                    </h3>`
 
         for (let intern of interns) {
-            generateListOfAllCards += internCardTemplate.generate(intern.id, intern.name, intern.picture)
+            generateListView += internCardTemplate.generate(intern.id, intern.name, intern.picture)
         }
 
-        generateListOfAllCards += '</section>'
+        generateListView += `</section>
+                            </div>`
 
-        return generateListOfAllCards
+        return generateListView
     }
 })
