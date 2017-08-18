7ce72d3b6495c160cd9b3bf461e3b9da32f0d6e9
Silvave <todor.vasilev@yahoo.com>
2017-08-18 03:55:30 +0300

    Added events to change questions intern questions

--- scripts/controllers/details-controller.js
+++ scripts/controllers/details-controller.js
@@ -1,25 +1,42 @@
 let detailsDependencies = [
 	'../data.js',
-	'..//views/details.js',
+	'../views/details.js',
+    '../templates/section-details-question-info.js'
 ]
 
-define(detailsDependencies, (data, detailsView) => {
+define(detailsDependencies, (data, detailsView, questionInfoTemplate) => {
 	let detailsController = {}
 	detailsController.render = e => {
 		let intern = data.getSingleIntern(e.target.value)
+        let internInfo = data.getAllInternQuestions(intern.id)
 
-		$('#root').html(detailsView(intern))
+		$('#root').html(detailsView(intern, internInfo.questions))
 	}
 	detailsController.attachListeners = listController => {
-		showListListener(listController, detailsController)
+        questionInfoListener()
+        showListListener(listController, detailsController)
 	}
 
-	return detailsController
+    function renderQuestionInfo(questionId) {
+        let questionData = data.getQuestionVideo(0, questionId)
+
+        let responseHtml = questionInfoTemplate(questionData.videoId, questionData.text)
+
+        $('.question-details').html(responseHtml)
+    }
+
+    function questionInfoListener() {
+        $('.question').on('click', null, null, (e) => {
+            renderQuestionInfo(e.currentTarget.attributes.value.value)
+        })
+    }
+
+    return detailsController
 })
 
 function showListListener(listController, detailsController) {
-	$('.intern-list-btn').on('click', null, null, () => {
-		listController.render()
-		listController.attachListeners(detailsController)
-	})
+    $('.close').on('click', null, null, () => {
+        listController.render()
+        listController.attachListeners(detailsController)
+    })
 }
