a4cd2cebd2a9311792f3e31815a0570c5e910577
Silvave <todor.vasilev@yahoo.com>
2017-08-19 00:19:18 +0300

    Added new public youtube videos and interns picture for demo purposes

--- scripts/database/data.js
+++ scripts/database/data.js
@@ -4,42 +4,42 @@ const interns = [
         name: 'Симона Георгиева',
         department: 'Digital Channels',
         position: 'Digital Channels Specialist',
-        picture: 'images/interns/hellion_portrait_roster.png'
+        picture: 'images/interns/cat1.jpg'
     },
     {
         id: 1,
         name: 'Люба Петрова',
         department: 'ЛП department',
         position: 'ЛП position',
-        picture: 'images/interns/grave_robber_portrait_roster.png'
+        picture: 'images/interns/cat2.jpg'
     },
     {
         id: 2,
         name: 'Кая Парашкевова',
         department: 'КП department',
         position: 'КП position',
-        picture: 'images/interns/vestal_portrait_roster.png'
+        picture: 'images/interns/cat3.jpg'
     },
     {
         id: 3,
         name: 'Георги Челенков',
         department: 'ГЧ department',
         position: 'ГЧ Position',
-        picture: 'images/interns/crusader_portrait_roster.png'
+        picture: 'images/interns/dog1.jpg'
     },
     {
         id: 4,
         name: 'Христо Димитров',
         department: 'ХД department',
         position: 'ХД position',
-        picture: 'images/interns/arbalest_portrait_roster.png'
+        picture: 'images/interns/dog2.jpg'
     },
     {
         id: 5,
         name: 'Дениз Стефанов',
         department: 'ДС department',
         position: 'ДС position',
-        picture: 'images/interns/houndmaster_portrait_roster.png'
+        picture: 'images/interns/dog3.jpg'
     },
 ]
 
@@ -51,31 +51,31 @@ const internQuestionInfo = [
             {
                 id: 0,
                 title: 'Защо избра да кандидатстваш в Теленор',
-                videoId: '1Wh8RzcQZr4',
+                videoId: 'n6ryV8WGctE',
                 text: 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis'
             },
             {
                 id: 1,
                 title: 'Какви са впечатленията ти за колегите ти',
-                videoId: 'oqNKD9YVe4U',
+                videoId: 'jpmWHPpaiso',
                 text: 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis'
             },
             {
                 id: 2,
                 title: 'В какъв проект участваш',
-                videoId: 'O1KW3ZkLtuo',
+                videoId: 'FkOyTwAE4HE',
                 text: 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis'
             },
             {
                 id: 3,
                 title: 'Разкажи ми за твой успех по време на стажа',
-                videoId: '79EvGkXHF9I',
+                videoId: 'ocB530rxUFA',
                 text: 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis'
             },
             {
                 id: 4,
                 title: 'Какво ще посъветваш бъдещите стажанти',
-                videoId: 'hvdW86xcnco',
+                videoId: 'es1D1lWI3x8',
                 text: 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis'
             }
         ]
