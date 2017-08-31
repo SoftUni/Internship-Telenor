3318b75f003020dcb5d84d9e8848f805ec461336
Todor Vasilev <todor.vasilev@yahoo.com>
2017-08-31 18:52:15 +0300

    Small Refactoring for index page templates

--- scripts/templates/list-view/section-cards.js
+++ scripts/templates/list-view/section-cards.js
@@ -7,7 +7,7 @@ define(['./intern-card.js'], internCardTemplate => {
                 </h2>`
 
         for (let intern of interns) {
-            generateInternsCards += internCardTemplate(intern.id, intern.name, intern.picture, intern.description)
+            generateInternsCards += internCardTemplate(intern)
         }
 
         generateInternsCards += `</section>`
