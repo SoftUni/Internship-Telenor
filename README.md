3be72d116b453b84540dae319247c08fabdc9ca6
Todor Vasilev <todor.vasilev@yahoo.com>
2017-09-13 14:10:23 +0300

    Adding app's link to github pages in README file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Internship-Telenor
 Internship informational app
+
+Link to app's github pages: [**Here**](https://softuni.github.io/Internship-Telenor/app/)
--- app/scripts/utils/clearAllBrowserIntervals.js
+++ app/scripts/utils/clearAllBrowserIntervals.js
@@ -1,5 +1,6 @@
 define(() => {
     return () => new Promise(() => {
+        // Clears all 100 previous interval plus the current one
         let currentIntervalId = setInterval(() => {}, 1000)
         let startIntervalId = currentIntervalId > 100 ? currentIntervalId - 100 : 0;
 
